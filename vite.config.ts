b06727992e7ba3f5f2c969f2d803396aa4822df5
Ronggur Habibun <ronggur.mh@gmail.com>
2026-02-01 22:43:32 +0700

    Update path and favicons

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { inspectAttr } from 'kimi-plugin-inspect-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/solar-system/',
   plugins: [inspectAttr(), react()],
   resolve: {
     alias: {
